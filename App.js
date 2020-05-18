4c9ea17ab3a33d17444470a57024ce41e1fbc53d
chjuca <chjuca@utpl.edu.ec>
2020-05-18 17:19:17 -0500

    Agregando pantalla de Calendar List al proyecto

--- App.js
+++ App.js
@@ -1,5 +1,17 @@
 import React from 'react';
-import LoginController from './LoginController';
+//import {createStackNavigator, createAppContainer} from 'react-navigation';
+//import LoginController from './LoginController';
+import CalendarListComponent from './src/calendar/CalendarListComponent';
 
-const App = () => {return (<LoginController/>);};
+// const LoginNavigator = createStackNavigator({
+//     login: {screen: LoginController}
+// })
+
+// const CalendarListNavigator = createStackNavigator({
+//     calendarList: {screen: CalendarListComponent}
+// })
+
+// export default createAppContainer(CalendarListNavigator);
+
+const App = () => {return (<CalendarListComponent/>);};
 export default App;
---
+++ src/calendar/CalendarListComponent.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+import { CalendarList } from 'react-native-calendars';
+
+export default class CalendarListComponent extends Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    render() {
+        return (
+            <CalendarList
+                // Callback which gets executed when visible months change in scroll view. Default = undefined
+                onVisibleMonthsChange={(months) => { console.log('now these months are visible', months); }}
+                // Max amount of months allowed to scroll to the past. Default = 50
+                pastScrollRange={50}
+                // Max amount of months allowed to scroll to the future. Default = 50
+                futureScrollRange={50}
+                // Enable or disable scrolling of calendar list
+                scrollEnabled={true}
+                // Enable or disable vertical scroll indicator. Default = false
+                showScrollIndicator={true}
+                // Dia presionado
+                onDayPress={(day) => {console.log(day.timestamp)}}
+
+            />
+        )
+    }
+
+
+}
