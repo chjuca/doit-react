723bf86a685369fc4f2f026408b588a081f89ed1
chjuca <chjuca@utpl.edu.ec>
2020-05-20 15:17:58 -0500

    Edicion del componente EventComponent

--- src/calendar/AgendaComponent.js
+++ src/calendar/AgendaComponent.js
@@ -14,10 +14,10 @@ export default class AgendaComponent extends Component {
         return (
             <Agenda
                 items={{
-                    '2020-05-22': [{ name: 'item 1 - any js object' }],
-                    '2020-05-23': [{ name: 'item 2 - any js object' }],
+                    '2020-05-22': [{ name: 'Exposicion Arquitectura',hour: '10:00AM -10:45AM', description: 'lorem ipsum dolor sit amet consectetur adipiscing elit,lorem ipsum dolor sit amet consectetur adipiscing elit', image: 'EA', color: '#33B679'}],
+                    '2020-05-23': [{ name: 'Deber Sistemas Operativos',hour: '10:00AM -10:45AM', description: 'lorem ipsum dolor sit amet consectetur adipiscing elit,lorem ipsum dolor sit amet consectetur adipiscing elit', image: 'DS', color: '#E67C73' }],
                     '2020-05-24': [],
-                    '2020-05-25': [{ name: 'item 3 - any js object' }, { name: 'any js object' }, { name: 'any js object' }]
+                    '2020-05-25': [{name: 'Presentar Informe', hour: '8:00AM -10:45AM', description: 'lorem ipsum dolor sit amet consectetur adipiscing elit,lorem ipsum dolor sit amet consectetur adipiscing elit', image: 'PI', color: '#3F51B5' }, { name: 'Control de Lectura ',hour: '10:00AM -10:45AM', description: 'lorem ipsum dolor sit amet consectetur adipiscing elit,lorem ipsum dolor sit amet consectetur adipiscing elit', image: 'CL', color: '#039BE5' }, { name: 'Presentacion Proyecto FIS',hour: '10:00AM -10:45AM', description: 'lorem ipsum dolor sit amet consectetur adipiscing elit,lorem ipsum dolor sit amet consectetur adipiscing elit', image: 'PP', color: '#7986CB'}]
                 }}
                 renderItem={(item, firstItemInDay) => {return (<EventComponent item = {item}/>);}}
                 onDayPress={(day) => { console.log('day pressed') }}
--- src/event/EventComponent.js
+++ src/event/EventComponent.js
@@ -11,13 +11,61 @@ export default class EventComponent extends Component {
     render() {
         return (
             <View style={styles.container}>
-                <Text>{this.props.item.name}</Text>
+                <View style={styles.containerRight}>
+                    <Text style={styles.hour}>{this.props.item.hour}</Text>
+                    <Text style={styles.name}>{this.props.item.name}</Text>
+                    <Text style={styles.description}>{this.props.item.description}</Text>
+                </View>
+                <View style={styles.containerLeft}>
+                    <Text style={[styles.image, {backgroundColor: this.props.item.color}]}>{this.props.item.image}</Text>
+                </View>
             </View>
-        )
+        );
     }
 }
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'yellow'
+        margin: 10,
+        backgroundColor: '#FFFFFF',
+        flexDirection: 'row',
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    containerRight: {
+        flex: 2,
+        flexDirection: 'column',
+        margin: 10,
+    },
+    containerLeft: {
+        flex: 0.75,
+    },
+    hour: {
+        fontFamily: 'Roboto',
+        fontSize: 15,
+        margin: 3,
+        color: '#737373'
+    },
+    name: {
+        fontFamily: 'Roboto',
+        fontWeight: 'bold',
+        fontSize: 20,
+        margin: 3,
+    },
+    description: {
+        fontFamily: 'Roboto',
+        fontSize: 13,
+        margin: 3,
+        color: '#929292'
+    },
+    image:{
+        borderRadius: 100,
+        textAlign: 'center',
+        textAlignVertical: 'center',
+        fontFamily: 'sans-serif-condensed',
+        width: 60,
+        height: 60,
+        color: 'white',
+        fontSize: 45
     }
 })
