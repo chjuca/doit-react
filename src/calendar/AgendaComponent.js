4f121039d1c8f6d2649b6cfb35fd6171bd3f4393
chjuca <chjuca@utpl.edu.ec>
2020-05-21 13:23:39 -0500

    Trabajando con Objetos

--- src/calendar/AgendaComponent.js
+++ src/calendar/AgendaComponent.js
@@ -7,19 +7,39 @@ export default class AgendaComponent extends Component {
 
     constructor(props) {
         super(props);
-        this.state = {}
+        this.state = {
+            events:
+                [{ name: 'Exposicion Arquitectura', hour: '10:00AM -10:45AM', description: 'lorem ipsum dolor sit amet consectetur adipiscing elit,lorem ipsum dolor sit amet consectetur adipiscing elit', image: 'EA', color: '#33B679', date: '2020-05-22' },
+                { name: 'Deber Sistemas Operativos', hour: '10:00AM -10:45AM', description: 'lorem ipsum dolor sit amet consectetur adipiscing elit,lorem ipsum dolor sit amet consectetur adipiscing elit', image: 'DS', color: '#E67C73', date: '2020-05-23' },
+                { name: 'Presentar Informe', hour: '8:00AM -10:45AM', description: 'lorem ipsum dolor sit amet consectetur adipiscing elit,lorem ipsum dolor sit amet consectetur adipiscing elit', image: 'PI', color: '#3F51B5', date: '2020-05-25' },
+                { name: 'Control de Lectura ', hour: '10:00AM -10:45AM', description: 'lorem ipsum dolor sit amet consectetur adipiscing elit,lorem ipsum dolor sit amet consectetur adipiscing elit', image: 'CL', color: '#039BE5', date: '2020-05-25' }],
+            items: {}
+        }
+    }
+
+    componentDidMount() {
+        let items = new Map();
+        for(let event of this.state.events){
+            if(items.has(event.date)){
+                items.get(event.date).push(event)
+            }else{
+                items.set(event.date, [event])
+            }
+        }
+        let itemsAux = new Object();
+        for (var [key, value] of items) {
+            itemsAux[key] = value;
+          }
+        this.setState({items : itemsAux}, ()=>{
+            console.log(this.state.items);
+        });
     }
 
     render() {
         return (
             <Agenda
-                items={{
-                    '2020-05-22': [{ name: 'Exposicion Arquitectura',hour: '10:00AM -10:45AM', description: 'lorem ipsum dolor sit amet consectetur adipiscing elit,lorem ipsum dolor sit amet consectetur adipiscing elit', image: 'EA', color: '#33B679'}],
-                    '2020-05-23': [{ name: 'Deber Sistemas Operativos',hour: '10:00AM -10:45AM', description: 'lorem ipsum dolor sit amet consectetur adipiscing elit,lorem ipsum dolor sit amet consectetur adipiscing elit', image: 'DS', color: '#E67C73' }],
-                    '2020-05-24': [],
-                    '2020-05-25': [{name: 'Presentar Informe', hour: '8:00AM -10:45AM', description: 'lorem ipsum dolor sit amet consectetur adipiscing elit,lorem ipsum dolor sit amet consectetur adipiscing elit', image: 'PI', color: '#3F51B5' }, { name: 'Control de Lectura ',hour: '10:00AM -10:45AM', description: 'lorem ipsum dolor sit amet consectetur adipiscing elit,lorem ipsum dolor sit amet consectetur adipiscing elit', image: 'CL', color: '#039BE5' }, { name: 'Presentacion Proyecto FIS',hour: '10:00AM -10:45AM', description: 'lorem ipsum dolor sit amet consectetur adipiscing elit,lorem ipsum dolor sit amet consectetur adipiscing elit', image: 'PP', color: '#7986CB'}]
-                }}
-                renderItem={(item, firstItemInDay) => {return (<EventComponent item = {item}/>);}}
+                items={this.state.items}
+                renderItem={(item, firstItemInDay) => { return (<EventComponent item={item} />); }}
                 onDayPress={(day) => { console.log('day pressed') }}
             />
         )
