e37978f7b2dbb95760c7e2aed2aecc2d0559ce66
chjuca <chjuca@utpl.edu.ec>
2020-05-26 17:38:26 -0500

    Commit antes de probar Deep links  sin errores

--- src/components/group/joinGroupComponent.js
+++ src/components/group/joinGroupComponent.js
@@ -1,11 +1,21 @@
-import { View, TextInput, Button, StyleSheet } from 'react-native';
+import { View, Text, Button, StyleSheet } from 'react-native';
+
 
 export default class JoinGroupComponent {
+    constructor(props) {
+        super(props);
+        this.state = {}
+    }
+
+    joinGroup(){
+        console.log('Hola desde JoinGroup!')
+    }
+
     render() {
         return (
             <View>
-
-                <Button title="Agregar" onPress={() => this.addGroup()} />
+                <Text> ¡Bienvenido! al grupo : name</Text>
+                <Button title="Unirme" onPress={() => this.joinGroup()} />
             </View>
         );
     }
